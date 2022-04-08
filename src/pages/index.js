a8800b8dbd62ac8bb177d4847d47556ff01055e1
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-08 17:51:36 +0530

    feat:files changed

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home/Home";
+import WishList from "./WishList/WishList";
+import ProductListing from "./ProductListing/ProductListing";
+import Cart from "./Cart/Cart";
+
+export {Home,WishList,ProductListing,Cart};
