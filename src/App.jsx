da0b12b1fa58380fa5ba2c5f50f32288197d3b35
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-09 17:13:14 +0530

    routes improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import { Home, WishList, ProductListing, Cart, Login, Signup } from "./pages";
 import { Navbar, Footer } from "./components/component";
+import { Home,WishList, ProductListing, Cart, Login, Signup } from "./pages";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Cart" element={<Cart />} />
@@ -14,7 +14,7 @@ function App() {
         <Route path="/Signup" element={<Signup />} />
         <Route path="/ProductListing" element={<ProductListing />} />
         <Route path="/Login" element={<Login />} />
-      </Routes>
+      </Routes> 
       <Footer />
     </div>
   );
