66521577c3c13e3a5236bff665a53746878ee9d3
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-10 19:13:25 +0530

    refactor-image added for kids cloth

--- src/assets/images/image.js
+++ src/assets/images/image.js
@@ -1,7 +1,7 @@
-import img1 from "../../assets/images/image-1.webp";
-import img2 from "../../assets/images/image-2.webp";
-import img3 from "../../assets/images/image-3.webp";
-import img4 from "../../assets/images/image-4.jpg";
-import img5 from "../../assets/images/image-5.jpg";
-
-export {img1, img2, img3, img4, img5};
+import img1 from "./image-1.webp";
+import img2 from "./image-2.webp";
+import img3 from "./image-3.webp";
+import img4 from "./image-4.jpg";
+import img5 from "./image-5.jpg";
+import img6 from "./image-6.jpg";
+export {img1, img2, img3, img4, img5, img6};
