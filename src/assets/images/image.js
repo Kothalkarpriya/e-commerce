7a2310b9e27383f14cef26cf77cc2e99642c5135
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-11 00:01:07 +0530

    refactor-changes with folder

--- src/assets/images/image.js
+++
@@ -1,7 +0,0 @@
-import img1 from "./image-1.webp";
-import img2 from "./image-2.webp";
-import img3 from "./image-3.webp";
-import img4 from "./image-4.jpg";
-import img5 from "./image-5.jpg";
-import img6 from "./image-6.jpg";
-export {img1, img2, img3, img4, img5, img6};
