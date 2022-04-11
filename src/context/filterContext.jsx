deeb7e5b0b4a21e36187bd33af7827bab23321f5
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-11 23:36:39 +0530

    feat-context and reducer added

---
+++ src/context/filterContext.jsx
@@ -0,0 +1,48 @@
+import {
+  filterReducer,
+  sortProd,
+  ratingFun,
+  CategoryHandler,
+  priceFun,
+} from "../components/component";
+import { GetProd } from "./prodContext";
+import { createContext, useContext, useReducer } from "react";
+
+const FilterContext = createContext();
+
+const filter =
+  (state, ...allFilters) =>
+  (data) => {
+    return allFilters.reduce((acc, currentVal) => {
+      return currentVal(state, acc);
+    }, data);
+  };
+
+const FilterContextPro = ({ children }) => {
+  const products = GetProd();
+
+  const [state, filterDispatch] = useReducer(filterReducer, {
+    sort: "",
+    rating: 5,
+    category: [],
+    maxPrice: 2000,
+  });
+
+  const filteredList = filter(
+    state,
+    sortProd,
+    ratingFun,
+    CategoryHandler,
+    priceFun
+  )(products);
+
+  return (
+    <FilterContext.Provider value={{ state, filterDispatch, filteredList }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+const useFilters = () => useContext(FilterContext);
+
+export { useFilters, FilterContextPro };
