e3a5f1d2b7d1cd66f771558cc7c77b0f6afa4aaf
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-11 23:36:22 +0530

    feat- components refactored and added

--- src/components/ProdCard.jsx
+++ src/components/ProdCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { AiTwotoneHeart } from "react-icons/ai";
+// import { BsStar } from "react-icons/bs";
 
-export default function ProdCard({ cardTitle, cardPrice, image }) {
+export default function ProdCard({ cardTitle, cardRating, cardPrice, image }) {
+  // const rounded = Math.round(cardRating * 2) / 2;
+  // const hasDecimal = rounded % 1 !== 0;
+  // const roundedWhole = Math.floor(rounded);
+  // const stars = hasDecimal ? roundedWhole + 1 : roundedWhole;
   return (
     <div>
       <div className="container">
@@ -15,6 +20,15 @@ export default function ProdCard({ cardTitle, cardPrice, image }) {
           <p className="small-text">{cardTitle}</p>
           <p className="large-text">Rs.{cardPrice}</p>
         </div>
+        {/* <div>
+          {[...Array(roundedWhole)].map((e, i) => (
+            <BsStar key={i} />
+          ))}
+          {hasDecimal ? <i class="fas fa-star-half-alt"></i> : ``}
+          {[...Array(5 - stars)].map((e, i) => (
+            <BsStar key={i} />
+          ))}
+        </div> */}
         <div className="card-buttons">
           <button type="submit" className="btn card-btn-items">
             Add to Cart
--- src/components/component.js
+++ src/components/component.js
@@ -2,5 +2,17 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Filter from "./Filter";
 import ProdCard from "./ProdCard";
+import filterReducer from "./filterRedcucer";
+import { CategoryHandler, sortProd, priceFun, ratingFun } from "./filterComp";
 
-export { Navbar, Footer, Filter, ProdCard };
+export {
+  Navbar,
+  Footer,
+  Filter,
+  ProdCard,
+  filterReducer,
+  CategoryHandler,
+  sortProd,
+  priceFun,
+  ratingFun,
+};
