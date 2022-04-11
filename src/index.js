b15784e25c8bbc05ea690c40c47ead7b27110047
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-11 23:37:05 +0530

    refactor- added filter on index page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { makeServer } from "./server";
+import { FilterContextPro } from "./context/filterContext";
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <FilterContextPro>
+        <App />
+      </FilterContextPro>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
