6895103d3698d3d7e008f2bea446f9b56d30404f
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-15 23:55:49 +0530

    feat-products added and rating changed for refer

--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { Filter, ProdCard } from "../../components/component";
+import { Filter, ProdCard, EmptyProd } from "../../components/component";
 import { GetProd } from "../../context/prodContext";
 
 export default function ProductListing() {
   const prodList = GetProd();
+  const prodMapping = () => {
+    prodList.map((item, index) => {
+      return (
+        <ProdCard
+        sr={index+1}
+          id={item._id}
+          cardTitle={item.title}
+          cardPrice={item.price}
+          image={item.imageUrl}
+        />
+      );
+    });
+  };
   return (
     <section className="main-section">
       <div className="product-container">
@@ -11,18 +24,11 @@ export default function ProductListing() {
         <article className="products">
           <div className="prod-heading upper-text">All Products</div>
           <div className="product-card">
-            <div className="card">
-              {prodList.map((item) => {
-                return (
-                  <ProdCard
-                    id={item._id}
-                    cardTitle={item.title}
-                    cardPrice={item.price}
-                    image={item.imageUrl}
-                  />
-                );
-              })}
-            </div>
+            {prodList.length ? (
+              <div className="card">{prodMapping()}</div>
+            ) : (
+              EmptyProd("PRODUCT")
+            )}
           </div>
         </article>
       </div>
--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -1,17 +1,44 @@
-  import React from "react";
-  import { WishCard } from "../../components/component";
+import React from "react";
+import { WishCard } from "../../components/component";
+import { useWishList } from "../../context/wishListContext";
+import { EmptyProd } from "../../components/component";
 
-  export default function WishList() {
-    return (
-      <main>
-        <section className="main-section">
-          <div className="prod-heading upper-text">My Wishlist</div>
-          <div className="wishlist-container">
-            <div className="card">
-              <WishCard />
-            </div>
+export default function WishList() {
+  const { state } = useWishList();
+  const wishList = state.wishListItems;
+
+  const wishListMapping = () => {
+    wishList.map((item, index) => {
+      return (
+        <WishCard
+          key={item.id}
+          id={item.id}
+          sr={index + 1}
+          prodName={item.title}
+          unitPrice={item.price}
+          img={item.imageUrl}
+          Action={"prod"}
+        />
+      );
+    });
+  };
+  return (
+    <main>
+      <section className="main-section">
+        <div className="prod-heading upper-text">My Wishlist</div>
+        <div className="wishlist-container">
+          <div className="card">
+            <WishCard
+              prodName={"Product Name"}
+              unitPrice={"Unit Price"}
+              img={"Image"}
+              Action={"Actions"}
+            />
+
+            {wishList.length ? wishListMapping() : EmptyProd("WISHLIST")}
           </div>
-        </section>
-      </main>
-    );
-  }
+        </div>
+      </section>
+    </main>
+  );
+}
