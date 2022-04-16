be855a45c9a082860282d3c0623b24c07f4c1dd2
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-16 18:15:52 +0530

    feat-cart card refactored and added function

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,8 +1,28 @@
-import React, {useEffect, useState} from "react";
-import {useWishList} from "../context/wishListContext";
-import {ItemInWishListCheck}from "../backend/utils/wishListReducer";
+import React, { useEffect, useState } from "react";
+import { useCart } from "../context/cartContext";
+import { useWishList } from "../context/wishListContext";
+import { ItemInWishListCheck } from "../backend/utils/wishListReducer";
 
-export default function CartCard({ title, img, price }) {
+export default function CartCard({ id, title, img, price, quantity }) {
+  const { dispatchWish } = useWishList();
+  const [count, setCount] = useState(quantity);
+  const { dispatchCart } = useCart();
+  const cartDefault = {
+    id: id,
+    imageUrl: img,
+    title: title,
+    price: price,
+    quantity: quantity,
+  };
+
+  useEffect(() => {
+    count < 1 || isNaN(count) ? setCount(1) : setCount(count);
+    dispatchCart({
+      type: "CART_QUANTITY",
+      payload: { id: id, quantity: count },
+    });
+  }, [count, dispatchCart, id]);
+
   return (
     <article className="prod-in-cart">
       <div className="image">
@@ -11,20 +31,54 @@ export default function CartCard({ title, img, price }) {
       <div className="prod-detail">
         <div className="text text-align-left">
           <p className="large-text">{title}</p>
-          <p className="large-text">
-            Rs.{price} 
-          </p>
+          <p className="large-text">Rs.{price * quantity}</p>
           <p className="grey-text">40% OFF</p>
           <p className="quant">
             <label>Quantity: </label>
-            <i className="fa fa-plus"></i>
-            <input type="number" min={0} value="1" />
-            <i className="fa fa-minus"></i>
+            <button
+              className="btn btn-primary"
+              onClick={() => setCount(count - 1)}
+            >
+              &nbsp;-&nbsp;
+            </button>
+            <input
+              type="text"
+              name="cartInput"
+              value={count}
+              onChange={(e) => {
+                setCount(e.target.value);
+              }}
+            />
+            <button
+              className="btn btn-primary"
+              onClick={() => setCount(count + 1)}
+            >
+              &nbsp;+&nbsp;
+            </button>
           </p>
         </div>
         <div className="button">
-          <button className="btns upper-text">Remove from Cart</button>
-          <button className="btns upper-text">Move to wishlist</button>
+          <button
+            className="btns upper-text"
+            onClick={() =>
+              dispatchWish({
+                type: "CART_REMOVE",
+                payload: cartDefault,
+              })
+            }
+          >
+            Remove from Cart
+          </button>
+          <button
+            className="btns upper-text"
+            onClick={
+              !ItemInWishListCheck(id)
+                ? () => dispatchWish({ type: "WISH_ADD", payload: cartDefault })
+                : () => {}
+            }
+          >
+            Add to wishlist
+          </button>
         </div>
       </div>
     </article>
