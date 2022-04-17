c668862897a56bace09bc58a9ba830eaf5f8875a
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-17 14:36:45 +0530

    feat-cart improved

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,42 +1,31 @@
 import React from "react";
-import {CartCard} from "../../components/component";
+import { CartCard, EmptyProd, CartCheckOut } from "../../components/component";
+import { useCart } from "../../context/cartContext";
 
 export default function Cart() {
+  const { state } = useCart();
+  const Cart = state.CartItems;
+
+  const CartMapping = () => {
+    Cart.map((item) => {
+      return (
+        <CartCard
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          price={item.price}
+          quantity={item.quantity}
+          image={item.imageUrl}
+        />
+      );
+    });
+  };
   return (
     <section className="main-section">
       <div className="prod-heading upper-text">My Cart(1)</div>
       <div className="cart-container">
-        <CartCard />
-        <article className="prod-price-detail">
-          <div className="price-heading upper-text large-text">
-            price details
-          </div>
-          <div className="table">
-            <table>
-              <tr>
-                <td className="cl-1">Price(1 Item)</td>
-                <td className="cl-2 text-align-right">Rs. 600</td>
-              </tr>
-              <tr>
-                <td className="cl-1">Discount</td>
-                <td className="cl-2 text-align-right">RS. -400</td>
-              </tr>
-              <tr>
-                <td className="cl-1">Delivery Charges</td>
-                <td className="cl-2 text-align-right">Rs. 60</td>
-              </tr>
-            </table>
-            <table className="table-2">
-              <tr className="total">
-                <td className="cl-1 upper-text ">Total amount</td>
-                <td className="cl-2 text-align-right">Rs.660</td>
-              </tr>
-            </table>
-            <p className="note">You will save Rs. 600 on this order</p>
-
-            <button className="btn upper-text">place order</button>
-          </div>
-        </article>
+        {Cart.length ? CartMapping() : EmptyProd("CARTLIST")}
+        <CartCheckOut />
       </div>
     </section>
   );
